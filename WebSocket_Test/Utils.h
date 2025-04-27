dcc8c47dfa794e2822c8660a45ebf7d1583f4095
Nate-Judd <nj424@drexel.edu>
2025-04-27 18:38:55 -0400

    Copied files for library dependencies

---
+++ WebSocket_Test/Utils.h
@@ -0,0 +1,17 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <string.h>
+#include <ArduinoJson.h>
+
+#define DEL 127
+
+bool isNumeric(String str);
+
+void flushInputBuffer();
+
+String getUserInput(String prompt);
+
+String generate_json_energy_record(char *transmission);
+
+#endif
